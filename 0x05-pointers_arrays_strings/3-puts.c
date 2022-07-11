279d752c11e49750e0810e396c25ef0f2c82c1f0
CynthiaM7 <cwanjiru907@gmail.com>
2022-07-11 11:01:16 -0700

    strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,14 @@
 
 /**
  * _puts -> this is a function that puts
- * @str: a param to _puts function
+ * @str: a parameter for _puts function
  */
+
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+		_putchar (*str);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
